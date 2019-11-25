1a4254ae5dea839b3c731986abb522fd51bf883b
Corey Lowe <corey@thoughtshop.com>
2019-11-25 13:28:29 -0800

    Update app.js

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,20 +1,19 @@
-import Cleave from 'cleave.js';
-import 'cleave.js/dist/addons/cleave-phone.ca';
+import 'intersection-observer'
+import Rellax from 'rellax'
+import Cleave from 'cleave.js'
+import 'cleave.js/dist/addons/cleave-phone.ca'
+import forEach from 'lodash/forEach'
 
-var cleavePhone = new Cleave('.input-phone', {
+const cleavePhone = new Cleave('.input-phone', {
   phone: true,
   delimiter: '-',
   phoneRegionCode: 'CA'
-});
+})
 
-var cleavePostal = new Cleave('.input-postal', {
+const cleavePostal = new Cleave('.input-postal', {
   blocks: [3, 3],
   uppercase: true
-});
-
-$('.callout').popover({
-  trigger: 'hover',
-});
+})
 
 $.validator.addMethod("valueMustEqual", function(value, element, arg){
   return arg == value;
@@ -44,23 +43,51 @@ $('#spark-registration-form').validate({
     'are_you_simulated': {
       valueMustEqual: ''
     }
+  },
+  errorPlacement: (error, element) => {
+    if (element.attr("name") === "answers[4388][answers]") {
+      element.parents('.form-group').append(error)
+    } else {
+      error.insertAfter(element);
+    }
   }
-});
+})
 
-$("input[name='answers[4389][answers]']").on("change", (e) => {
+$("select[name='answers[4389][answers]']").on("change", (e) => {
+  console.log('change')
   if (e.currentTarget.value === "Yes") {
     $("#realtor-details").collapse("show");
   } else {
     $("#realtor-details").collapse("hide");
   }
-});
+})
 
-var formUrl = "https://spark.re/shape-marketing-corp/rc-1/register/rc1-registration";
+const sections = document.querySelectorAll('.reveal');
 
-$(function() {
-  var useJS = $('html').hasClass('js');
-  
+const observer = new IntersectionObserver(sections => {
+  forEach(sections, section => {
+    if (section.isIntersecting) {
+      section.target.classList.add('in-viewport')
+    }
+  })
+}, {
+  threshold: 0.4
+})
+
+forEach(sections, section => {
+  observer.observe(section);
+})
+
+$(() => {
+
+  const useJS = $('html').hasClass('js');
+
   if (useJS) {
+    const formUrl = "https://spark.re/shape-marketing-corp/rc-1/register/rc1-registration";
     $("#spark-registration-form").attr("action", formUrl);
   }
-});
+
+  const rellax = new Rellax('.rellax');
+})
+
+
