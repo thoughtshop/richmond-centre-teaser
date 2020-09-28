6b817b611f36c36a47d252acb387ab4e436cd19b
Corey Lowe <coreylowe@me.com>
2020-09-28 15:58:19 -0700

    Update to submit form to custom script

--- src/scripts/disclosure.js
+++ src/scripts/disclosure.js
@@ -2,6 +2,7 @@ const dorts = document.querySelector('#dorts');
 const drup = document.querySelector('#drup');
 const dortsConfirm = document.querySelector('#dorts-confirmation');
 const drupConfirm = document.querySelector('#drup-confirmation');
+const emailInput = document.querySelector('input[name="contact[email]"]');
 const agreeBtn = document.querySelector('#agree-btn');
 
 function enableRadioInput() {
@@ -12,8 +13,20 @@ function enableRadioInput() {
   document.querySelector(`#${this.id} + .tooltip`).classList.add('hidden');
 }
 
+function isEmailValid() {
+  if (emailInput.value !== '' && /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(emailInput.value)) return true;
+
+  return false;
+}
+
 function updateAgreeBtnState() {
-  if (!(dorts.disabled) && !(drup.disabled) && dortsConfirm.checked && drupConfirm.checked) {
+  if (
+    !dorts.disabled
+    && !drup.disabled
+    && dortsConfirm.checked
+    && drupConfirm.checked
+    && isEmailValid()
+  ) {
     agreeBtn.classList.remove('disabled');
   }
 }
@@ -22,3 +35,51 @@ dorts.addEventListener('click', enableRadioInput);
 drup.addEventListener('click', enableRadioInput);
 dortsConfirm.addEventListener('change', updateAgreeBtnState);
 drupConfirm.addEventListener('change', updateAgreeBtnState);
+emailInput.addEventListener('change', updateAgreeBtnState);
+
+$('#disclosure-form').validate({
+  rules: {
+    'dorts-confirmation': 'required',
+    'drup-confirmation': 'required',
+    'contact[email]': {
+      required: true,
+      email: true
+    },
+  },
+  errorPlacement: (error, element) => {
+    if (element.attr('type') === 'radio') {
+      element.parents('.disclosure-check-item').append(error);
+    } else {
+      error.insertAfter(element);
+    }
+  }
+});
+
+$('#agree-btn').on('click', (e) => {
+  e.preventDefault();
+
+  if ($('#disclosure-form').valid()) {
+    const form = document.getElementById('disclosure-form');
+    const formData = new FormData(form);
+
+    const fetchUrl = 'assets/disclosure.php';
+
+    fetch(fetchUrl, {
+      method: 'POST',
+      body: JSON.stringify(Object.fromEntries(formData.entries())),
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      }
+    })
+      .then((response) => {
+        if (!response.ok) throw Error(response.statusText);
+        return response.json();
+      })
+      .then((data) => {
+        if (data.success) {
+          document.location.href = 'https://app.acuityscheduling.com/schedule.php?owner=18897016&appointmentType=16952235';
+        }
+      });
+  }
+});
